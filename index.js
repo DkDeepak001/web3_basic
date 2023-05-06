f210aaff4008cb6c70aea3718f4628998dfa6f63
DkDeepak001 <Deepakeswar001@gmail.com>
2023-05-06 14:56:42 +0530

    getting current nlock number

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+import { InfuraProvider } from "ethers";
+
+const network = process.env.NETWORK || "homestead";
+const provider = new InfuraProvider(network, process.env.INFURA_API_KEY);
+
+const blockNumber = await provider.getBlockNumber();
+console.log("Current block number: ", blockNumber);
