100cbba9fd95a48dc088f70502f3837a66318796
DkDeepak001 <Deepakeswar001@gmail.com>
2023-05-07 07:33:26 +0530

    getting the balance of random address

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import { InfuraProvider } from "ethers";
+import { InfuraProvider, formatEther } from "ethers";
 
 const network = process.env.NETWORK || "homestead";
+//creating provider
 const provider = new InfuraProvider(network, process.env.INFURA_API_KEY);
 
+//getting current block number
 const blockNumber = await provider.getBlockNumber();
 console.log("Current block number: ", blockNumber);
+
+//getting randpo, address from blockchatin
+const address = "0xb78c885b9284821831fd555762f05c3296344077";
+//getting balance of address
+const balance = await provider.getBalance(address);
+
+//formatting balance
+console.log("Balance: ", formatEther(balance));
